85d54e7cbe956f9958f973f94b1eb914e4feb911
Unnikuttankk <87984213+Unnikuttankk@users.noreply.github.com>
2021-08-09 14:30:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Developers', url='https://t.me/filmgallery0999'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/filmgallery0999')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
